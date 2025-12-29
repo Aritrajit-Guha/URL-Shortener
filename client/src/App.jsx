7aee372070b1f9cbce0a510d10662a1124f22870
Aritrajit Guha <aritrajitguha123@gmail.com>
2025-12-29 14:09:31 +0530

    IDK2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,24 @@
 // App.jsx
 import "./App.css";
-import { Routes, Route } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
-import Home from './components/Home';
-import ShortUrl from './components/ShortUrl';
+import Home from './components/Home'
+import ShortUrl from './components/ShortUrl'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+const FRONTEND_URL = import.meta.env.VITE_APP_FRONTEND_URL;
+console.log(FRONTEND_URL);
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/shorturl" element={<ShortUrl />} />
-    </Routes>
+    <BrowserRouter basename={new URL(FRONTEND_URL).pathname}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/shorturl" element={<ShortUrl />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const PORT = 8000;
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL,
-    'http://localhost:5174'
+    'http://localhost:5173'
   ],
   credentials: true
 }));
